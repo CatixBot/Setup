9d9f8b3634438ae41b5007ddf375dbf31aae6bde
MikhailUskin <wurty@mail.ru>
2021-01-22 00:18:44 +0300

    add: docker compose for all nodes

---
+++ stop_nodes.bat
@@ -0,0 +1,13 @@
+@echo off
+
+set ROS_DOCKER_ADDRESS=%1
+if "%ROS_DOCKER_ADDRESS%"=="" (
+    echo ROS docker host IP address is expected in the first argument
+    exit /b 1
+)
+
+set DOCKER_HOST=tcp://%ROS_DOCKER_ADDRESS%:2375
+echo Docker Host: %DOCKER_HOST%
+
+docker-compose down
+exit /b 0
